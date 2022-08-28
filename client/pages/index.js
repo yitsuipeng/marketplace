0cc9f1e2dad255a63da5b336b665e9e3af230a5f
Mac <mac@Macde-MacBook-Pro.local>
2022-08-28 23:32:50 +0800

    [feature] add signup page template

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
+    axios.get('/api/users/currentuser').catch((err) => {
+      console.log(err.message);
+    });
+   
+    return <h1>Landing Page</h1>;
+  };
+
+export default LandingPage;
