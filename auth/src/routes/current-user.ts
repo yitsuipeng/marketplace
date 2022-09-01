f8e219ce20adbcc3bb1e8b11fb1d440c0e5ca7c7
Mac <mac@Macde-MacBook-Pro.local>
2022-09-01 16:17:16 +0800

    move out shared library

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@ytmarketplace/common';
 
 const router = express.Router();
 
