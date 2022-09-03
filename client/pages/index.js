bdc1b9a0a2889c03961340fb2027654482d60b20
Mac <mac@Macde-MacBook-Pro.local>
2022-09-04 02:56:28 +0800

    [feature] add item page frontend

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,41 @@
-import buildClient from "../api/build-client";
+import Link from 'next/link';
 
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-		<h1>You are signed in</h1>
-	) : (
-		<h1>You are NOT signed in</h1>
-	);
+const LandingPage = ({ currentUser, items }) => {
+  const itemList = items.map((item) => {
+    return (
+      <tr key={item.id}>
+        <td>{item.title}</td>
+        <td>{item.price}</td>
+        <td>
+          <Link href="/items/[itemId]" as={`/items/${item.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Items</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{itemList}</tbody>
+      </table>
+    </div>
+  );
 };
 
-LandingPage.getInitialProps = async (context) => {
-	console.log('LANDING PAGE!');  
-	const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-	
-	return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/items');
+
+  return { items: data };
 };
 
 export default LandingPage;
---
+++ client/pages/items/new.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+const NewItem = () => {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/items',
+    method: 'post',
+    body: {
+      title,
+      price,
+    },
+    onSuccess: () => Router.push('/'),
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    setPrice(value.toFixed(2));
+  };
+
+  return (
+    <div>
+      <h1>Create an Item</h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Price</label>
+          <input
+            value={price}
+            onBlur={onBlur}
+            onChange={(e) => setPrice(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        {errors}
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewItem;
