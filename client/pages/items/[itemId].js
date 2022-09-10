74b06fb7f66b7318c4cb4884a1cc116a142dcee9
Mac <mac@Macde-MacBook-Pro.local>
2022-09-10 09:57:08 +0800

    [feature] add orders page basic template

--- client/pages/items/[itemId].js
+++ client/pages/items/[itemId].js
@@ -1,5 +1,34 @@
-const ItemDetail = () => {
-    return <div>Item Detail</div>;
+import Router from 'next/router';
+import userRequest from '../../hooks/use-request';
+
+const ItemDetail = ({ item }) => {
+    const { doRequest, errors } = userRequest({
+        url: '/api/orders',
+        method: 'post',
+        body: {
+            itemId: item.id
+        },
+        onSuccess: (order) => 
+            Router.push('/orders/[orderId]', `/orders/${order.id}`)
+    });
+
+    return (
+        <div>
+            <h1>{item.title}</h1>
+            <h4>Price: {item.price}</h4>
+            {errors}
+            <button onClick={doRequest} className="btn btn-primary">
+                Purchase
+            </button>
+        </div>
+    );
+};
+
+ItemShow.getInitialProps = async (context, client) => {
+    const { itemId } = context.query;
+    const { data } = await client.get(`/api/items/${itemId}`)
+
+    return { item: data };
 };
 
 export default ItemDetail;
