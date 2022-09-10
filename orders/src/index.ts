35fa77e555b937841c24a6fa5a4deded88ae926f
Mac <mac@Macde-MacBook-Pro.local>
2022-09-10 16:49:41 +0800

    [bugfix] fix the order that nats-wrapper connect in index.ts

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -23,9 +23,9 @@ const start = async () => {
 
   try {
     await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID, 
-      process.env.NATS_URL, 
-      process.env.NATS_CLUSTER_ID
+      process.env.NATS_URL
     );
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
